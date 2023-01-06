24f2d5c99a6ed500a005009ae41a53c800438a2b
milliax <wei@milliax.me>
2023-01-07 03:34:08 +0800

    faced serious issue handling picture path

--- components/settings.tsx
+++ components/settings.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useEffect, useState, useRef } from "react"
+import { updateDirectory } from "../redux/reducers/settingsSlice";
 
 const availableMethods = ["資料夾設定"]
 
@@ -8,8 +9,8 @@ export default function Settings() {
     return (
         <div className="overflow-auto h-full w-full scrollbar flex flex-row justify-between scrollbar">
             <div className="w-32 flex flex-col space-y-2">
-                {availableMethods.map((text,cnt) => (
-                    <div className="self-center bg-gray-100 hover:bg-gray-200 w-full py-1 text-center rounded-xl cursor-pointer" key={text} onClick={()=>{
+                {availableMethods.map((text, cnt) => (
+                    <div className="self-center bg-gray-100 hover:bg-gray-200 w-full py-1 text-center rounded-xl cursor-pointer" key={text} onClick={() => {
                         setMethodSelected(cnt);
                     }}>{text}</div>
                 ))}
@@ -21,10 +22,27 @@ export default function Settings() {
     )
 }
 
-function FolderSelection(){
-    return(
-        <div>
-            Folder Select
+function FolderSelection() {
+    const [directorySelected, setDirectorySelected] = useState<string>("");
+    const folderInput = useRef();
+
+    useEffect(() => {
+        // TODO: pull directory path from redux
+    }, [])
+
+    return (
+        <div className="space-y-2">
+            <div>Folder Selector</div>
+            <div className="flex flex-wrap space-x-3">
+                <div>
+                    Now selected Folder
+                </div>
+                <div>D:\</div>
+            </div>
+            <div className="bg-red-200 px-3 py-1 w-20 text-center rounded-full select-none hover:bg-red-300 cursor-pointer" onClick={() => {
+                // updateDirectory()
+            }}>變更</div>
+            <input type="file" directory="" webkitdirectory="" ref={folderInput} />
         </div>
     )
 }
